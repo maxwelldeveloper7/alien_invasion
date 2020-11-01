57bee931f08481850df48ac3231b7a0c04cb34dc
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-11-01 15:17:21 -0300

    pagina 315 - criando a frota

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -11,16 +12,18 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width,ai_settings.screen_heght))
 	pygame.display.set_caption("Alien Invasion")
-	# Cria uma espaçonave
+	# Cria uma espaçonave, um grupo de projéteis e um grupo de alienígenas
 	ship = Ship(ai_settings, screen)
-	# Cria um grupo no qual serão armazenados os projéteis
 	bullets = Group()
+	aliens = Group()
+	# Cria uma frota de alienígenas
+	gf.create_fleet(ai_settings, screen, aliens)
 	#Inicia o laço principal do jogo
 	while True:
-		gf.check_events(ai_settings, screen, ship, bullets)
+		gf.check_events(ai_settings, screen, ship, alien, bullets)
 		ship.update()
 		bullets.update()
 		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,9 @@
 from bullet import Bullet
 import sys
 import pygame
+from alien import Alien
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, ship, alien, bullets):
 	"""Responde a eventos de pressionamento de teclas e de mouse"""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -18,6 +19,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 			ship.moving_left = True
 		elif event.key == pygame.K_SPACE:
 			fire_bullet(ai_settings, screen, ship, bullets)
+		elif event.key == pygame.K_q:
+			sys.exit()
 
 def check_keyup_events(event, ship):
 	if event.type == pygame.KEYUP:
@@ -26,13 +29,14 @@ def check_keyup_events(event, ship):
 		elif event.key == pygame.K_LEFT:
 			ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
 	"""Atualiza as imagens na tela e alterna para nova tela."""
 	# Redesenha a tela a cada passagem pelo laço
 	screen.fill(ai_settings.bg_color)
 	for bullet in bullets.sprites():
 		bullet.draw_bullet()
 	ship.blitme()
+	alien.blitme()
 	# Deixa a tela mais recente visível
 	pygame.display.flip()
 
@@ -50,3 +54,18 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 	if len(bullets) < ai_settings.bullets_allowed:
 		new_bullet = Bullet(ai_settings, screen, ship)
 		bullets.add(new_bullet)
+
+def create_fleet(ai_settings, screen, aliens):
+	"""Cria uma fota completa de alienígenas."""
+	# Cria um alienígena e calcula o número de alienígenas em uma linha
+	# O espaçamento entre os alienígenas é igual à largura de um alieníg
+	alien = Alien(ai_settings, screen)
+	alien_width = alien.rect.width
+	avalilable_space_x = ai_settings.screen_width - 2 * alien_width
+	numbers_aliens_x = int(avalilable_space_x / (2 * alien_width))
+	# Cria a primeira linha de alienígenas
+	for alien_number in range(numbers_aliens_x):
+		# Cria um alienígena e o posiciona na linha
+		alien = Alien(ai_settings, screen)
+		alien.x = alien_width + 2
+		aliens.add(alien)
